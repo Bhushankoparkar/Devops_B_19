9253d96a42b8010d81e24ff3eeca7e28c84fa44a
swapnil <swapnibrada@gmail.com>
2022-06-30 02:42:01 +0000

    today

--- script.sh
+++ script.sh
@@ -1,2 +1,31 @@
-#!/bin/bash
+name: Playbook1
+  hosta: all
+  vars: 
+    URL: www.google.com
+    URL_i: www.google.com/images
+  tasks:
+  - name: Print messages
+    debug:
+      msg:
+        welcome to cloudblitz
+        my name is swapnil
+----------------------------------------------------------------
+  - name: Print messages_1
+      debug:
+        msg: "{{URL}}"
+
+  - name: Print messages_2
+      debug:
+        msg: "{{URL_i}}"
+
+  - name: Print messages_3
+      debug:
+        msg: "{{URL}}"
+      vars:
+        URL: local.google.com
+----------------------------------------------
+  vars_files: <file_name.yml(URL_2: abc.google.com)>
+  -name: Print messages_4
+      debug:
+        msg: "{{URL_2}}"
 
