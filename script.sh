0f09e16f4c3b531871bbf98e39e564e7d00b2c2d
swapnil <swapnibrada@gmail.com>
2022-07-01 02:42:03 +0000

    munna

--- script.sh
+++ script.sh
@@ -28,4 +28,39 @@ name: Playbook1
   -name: Print messages_4
       debug:
         msg: "{{URL_2}}"
+-----------------------------------------------------
 
+	#pass variables with help of prompt
+- name: Playbook2
+  hosts: all
+  vars:
+    URL: Global
+
+  vars_files:
+    - data.yml
+
+  vars_prompt:
+    - name: URL
+      prompt: "ENTER URL"
+      private: no
+
+  tasks:
+    - name: Print messages_2
+      debug:
+        msg: "{{URL}}"
+
+
+
+	-------------------------------------------------------------------------------------------
+	- name: Playbook1
+  hosts: all
+  vars:
+    URL: www.google.com
+    URL_i: www.google.com/images
+  tasks:
+    - name: Print messages
+    - ping:
+      debug:
+        msg:
+          welcome to cloudblitz
+          my name is swapnil
